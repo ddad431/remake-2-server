c11293087decd9afff17fc7e54386233751af0a1
ddad431 <i.lihao@qq.com>
2025-03-25 01:30:20 +0800

    feat: update user service

--- services/modules/user.js
+++ services/modules/user.js
@@ -20,9 +20,46 @@ function useUserService() {
         return userInfo;
     }
 
+    function getUserList() {
+        const users = readFile(usersFilePath);
+        const userList = users.map(({password, ...userInfo}) =>  userInfo);
+
+        return userList;
+    }
+
+    function addUser(info) {
+        const users = readFile(usersFilePath);
+        const id = users[users.length - 1].id + 1;
+        const avators = [ "/images/avator-admin.png", "/images/avator-user.png", ];
+        const avator = avators[Math.round(Math.random())];
+        const password = info?.username + '123';
+
+        users.push({id, avator, password, ...info});
+        writeFile(usersFilePath, users);
+    }
+
+    function deleteUser(ids) {
+        const users = readFile(usersFilePath);
+        const userList = users.filter(user => !ids.includes(user.id));
+
+        writeFile(usersFilePath, userList);
+    }
+
+    function updateUserInfo(info) {
+        const users = readFile(usersFilePath);
+        const user = users.find(value => value.id === info.id);
+
+        Object.assign(user, info);
+        writeFile(usersFilePath, users);
+    }
+
     return {
         isValidUser,
-        getUserInfo
+        getUserInfo,
+        getUserList,
+        addUser,
+        deleteUser,
+        updateUserInfo,
     }
 }
 
