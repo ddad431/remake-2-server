49da311dc04200cb92c3798bf43ec1f6153d3ec3
ddad431 <i.lihao@qq.com>
2025-03-30 20:30:59 +0800

    feat(service): update role service

--- services/modules/role.js
+++ services/modules/role.js
@@ -19,9 +19,43 @@ function useRoleService() {
         return menus;
     }
 
+    function getRoleList() {
+        const roles = readFile(rolesFilePath);
+        const roleList = roles.map(({menus, ...info}) => info);
+
+        return roleList;
+    }
+
+    function addRole(info) {
+        const roles = readFile(rolesFilePath);
+        const id = roles.length > 0 ? roles[roles.length-1].id + 1 : 1;
+
+        roles.push({id, ...info});
+        writeFile(rolesFilePath, roles);
+    }
+
+    function deleteRole(ids) {
+        const roles = readFile(rolesFilePath);
+        const roleList = roles.filter(role => !ids.includes(role.id));
+
+        writeFile(rolesFilePath, roleList);
+    }
+
+    function updateRole(info) {
+        const roles = readFile(rolesFilePath);
+        const roleId = roles.findIndex(role => role.id === info.id);
+
+        roles[roleId] = info;
+        writeFile(rolesFilePath, roles);
+    }
+
     return {
         isValidRole,
-        getRoleMenu
+        getRoleMenu,
+        getRoleList,
+        addRole,
+        deleteRole,
+        updateRole,
     };
 }
 
